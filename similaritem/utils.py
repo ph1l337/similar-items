68d200d65f14f65614447be265a6651dbd92c179
Philipp Eisen <philipp@n179-p174.kthopen.kth.se>
2016-11-11 19:34:23 +0100

    more test cases for creating shingles

--- similaritem/utils.py
+++ similaritem/utils.py
@@ -3,15 +3,15 @@ import re
 
 
 def create_shingles_from_file(filepath, shingle_size):
-
     shingles = set()
     left_over = ''
     with open(filepath, 'r') as fp:
         for line in fp:
             working_line = left_over + line
+            working_line = re.sub('\n', ' ', working_line)
             working_line = re.sub('\t', ' ', working_line)
             working_line = re.sub('[ ]{2,}', ' ', working_line)
-            for i in range(len(working_line) - shingle_size):
+            for i in range(len(working_line) - (shingle_size - 1)):
                 shingles.add(working_line[i:i + shingle_size])
                 left_over = working_line[-(shingle_size - 1):]
 
@@ -23,7 +23,6 @@ def hash_shingles(shingles, maxi):
 
 
 def create_shingles_signature(hashed_shingles, hash_funcs):
-
     signature = [sys.maxsize for _ in range(len(hash_funcs))]
     for shingle_hash in hashed_shingles:
         signature = [min(hfunc(shingle_hash), val) for hfunc, val in zip(hash_funcs, signature)]
